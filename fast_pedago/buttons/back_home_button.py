2fbddb1f9e185beb673d368261d3a1fdafc8872a
Marien Giordani <giordanimarien@gmail.com>
2024-05-15 13:54:45 +0200

    tests on bottom buttons

--- fast_pedago/buttons/back_home_button.py
+++ fast_pedago/buttons/back_home_button.py
@@ -1,4 +1,5 @@
 import ipywidgets as widgets
+import ipyvuetify as v
 
 
 def get_back_home_button():
@@ -7,5 +8,10 @@ def get_back_home_button():
     back_home_button.icon = "fa-home"
     back_home_button.layout.width = "auto"
     back_home_button.layout.height = "auto"
+
+    back_home_button = v.Btn(
+        children = [v.Icon(children=["fa-home"])],
+        icon = True
+    )
 
     return back_home_button
--- fast_pedago/buttons/info_button.py
+++ fast_pedago/buttons/info_button.py
@@ -3,6 +3,7 @@
 # Copyright (C) 2022 ISAE-SUPAERO
 
 import ipywidgets as widgets
+import ipyvuetify as v
 
 from IPython.display import display
 
@@ -10,13 +11,38 @@ from IPython.display import display
 display(widgets.HTML("""<style>.white-vbox {background-color: white;}</style>"""))
 
 
+def get_base_info_button():
+
+    # Creating and instantiating an empty info button
+    info_button = v.Tooltip(
+        top = True, 
+        max_width = "50%",
+
+        v_slots = [{
+            'name': 'activator',
+            'variable': 'tooltip',
+            'children': v.Html(
+                tag="div", 
+                v_on = 'tooltip.on',
+                class_ = "d-inline-block",
+                children=[v.Btn(
+                    children = [v.Icon(children=["fa-info-circle"])],
+                    icon = True,
+                    disabled = True
+                )]
+            ),
+        }], 
+        children = ['']
+    )
+
+    return info_button
+
+
 def get_main_menu_info_button():
 
     # Creating and instantiating an info button
-    info_button = widgets.Button(description="")
-    info_button.icon = "fa-info-circle"
-    info_button.layout.width = "auto"
-    info_button.tooltip = (
+    info_button = get_base_info_button()
+    info_button.children[0] = (
         "Welcome to the training branch of FAST-OAD.\n This is the main menu which can lead you "
         "to the different activities to be performed. You'll also find some links to the source "
         "code of FAST-OAD and its plugins."
@@ -28,10 +54,8 @@ def get_main_menu_info_button():
 def get_sensitivity_analysis_info_button():
 
     # Creating and instantiating an info button
-    info_button = widgets.Button(description="")
-    info_button.icon = "fa-info-circle"
-    info_button.layout.width = "auto"
-    info_button.tooltip = (
+    info_button = get_base_info_button()
+    info_button.children[0] = (
         "This is the sensitivity analysis part of the training branch.\n"
         "In this part, you'll study the influence of a few select aircraft design "
         "parameters on the mass, aerodynamics and performances of the aircraft "
@@ -41,12 +65,10 @@ def get_sensitivity_analysis_info_button():
 
 
 def get_single_process_selection_info_button():
+
     # Creating and instantiating an info button
-    info_button = widgets.Button(description="")
-    info_button.icon = "fa-info-circle"
-    info_button.layout.width = "auto"
-    info_button.layout.height = "auto"
-    info_button.tooltip = (
+    info_button = get_base_info_button()
+    info_button.children[0] = (
         "- Select a sizing process in the dropdown menu to make the corresponding display appear \n"
         '- Select "None" to clear the display'
     )
@@ -57,11 +79,8 @@ def get_single_process_selection_info_button():
 def get_multiple_process_selection_info_button():
 
     # Creating and instantiating an info button
-    info_button = widgets.Button(description="")
-    info_button.icon = "fa-info-circle"
-    info_button.layout.width = "auto"
-    info_button.layout.height = "auto"
-    info_button.tooltip = (
+    info_button = get_base_info_button()
+    info_button.children[0] = (
         "- Select a sizing process in the dropdown menu to add the corresponding aircraft to the "
         "display  \n"
         '- Select "None" to clear the display'
